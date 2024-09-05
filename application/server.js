f050be7efef879a0b7b1c7fd1958fc44759fcccf
travmonkey <t.coding@batk.me>
2024-09-05 12:50:38 -0600

    Rework directory structure

--- application/server.js
+++ application/server.js
@@ -1,15 +1,16 @@
 // Load node modules
 const express = require('express');
+const path = require('path');
 const ejs = require('ejs');
 // Init express
 const app = express();
 // Rendering static files
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.render('public/demo');
+  res.render('demo');
 });
 
 app.listen(8080);
