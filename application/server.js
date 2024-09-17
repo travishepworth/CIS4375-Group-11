bc06a493f4e038a54ccba6d679b9d26e99545c4f
travmonkey <travishepworth14@gmail.com>
2024-09-17 14:08:29 -0500

    Create initial sidebar

--- application/server.js
+++ application/server.js
@@ -2,15 +2,20 @@
 const express = require('express');
 const path = require('path');
 const ejs = require('ejs');
+// const bootstrap = require('bootstrap');
+// import "./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+// import 'bootstrap';
 // Init express
 const app = express();
 // Rendering static files
+// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'node_modules/bootstrap/dist/')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.render('demo');
+  res.render('sidebar');
 });
 
 app.listen(8080);
