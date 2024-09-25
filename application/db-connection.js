289d2261ed907f8648a43da5490bc337f7caa982
hamzasaeed27 <hsaeed6@uh.edu>
2024-09-25 14:06:41 -0500

    Initialize DB connection

---
+++ application/db-connection.js
@@ -0,0 +1,37 @@
+const mysql = require('mysql2');
+
+// Create a connection to the database
+const connection = mysql.createConnection({
+  host: 'test-database.ceti305yyydb.us-east-1.rds.amazonaws.com',      // e.g., localhost or AWS RDS endpoint
+  user: 'admin',           // e.g., root
+  password: 'tc4375team11',       // your database password
+  database: 'technological_constructions'   // the name of your database
+});
+
+// Connect to the database
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to the database:', err);
+    return;
+  }
+  console.log('Connected to the database.');
+});
+
+// Example query (optional)
+connection.query('SELECT * FROM Country;', (err, results, fields) => {
+  console.log('Query initiated...');
+  if (err) {
+    console.error('Error executing query:', err);
+    return;
+  }
+  console.log('Query results:', results);
+});
+
+// Close the connection (optional)
+connection.end((err) => {
+  if (err) {
+    console.error('Error closing the connection:', err);
+    return;
+  }
+  console.log('Database connection closed.');
+});
