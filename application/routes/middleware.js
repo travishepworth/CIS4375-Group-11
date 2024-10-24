d4e513e1f019e7f6425676953f2175fe6e7c0034
Cdriver4 <cdriver4@cougarnet.uh.edu>
2024-10-24 18:09:28 -0500

    Employee Form

---
+++ application/routes/middleware.js
@@ -0,0 +1,12 @@
+const session = require("express-session");
+
+const isAuthenticated = (req, res, next) => {
+  if (req.session.isAuthenticated) {
+    next();
+  } else {
+    console.log("NO AUTH");
+    res.redirect("/");
+  }
+};
+
+module.exports = isAuthenticated;
